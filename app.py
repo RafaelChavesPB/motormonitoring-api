5cec010be6b0cc7dd54d01dd99557e37c9dbfd33
Rafael Cavalcante chaves <rafael.chaves@polodeinovacao.ifpb.edu.br>
2023-03-04 16:49:00 -0300

    Estabelecendo comunicação com o frontend

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask, request
 from flask_socketio import SocketIO, emit
+from flask_cors import CORS
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+CORS(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route('/', methods = ['GET'])
 def get_index():
-    return 'Hello World'
+    return 'Olá este é o backend da aplicação motormonitoring'
+
+@app.route('/', methods = ['POST'])
+def get_post():
+    socketio.emit('incoming_data', {'message': 'hello world'})
+    return 'sad'
 
 if __name__ == '__main__':
     socketio.run(app)
